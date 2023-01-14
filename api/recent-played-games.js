9bce5046ed550244b9de2837f081d9a8e010cf82
skezam <sammynguyen92000@gmail.com>
2023-01-14 14:00:55 +0100

    ajout du api_cors

--- api/recent-played-games.js
+++ api/recent-played-games.js
@@ -1,7 +1,12 @@
 import { getRecentPlayedGames } from '../lib/steam';
+import { CORS_HOST } from '../consts';
 
 export default async function recentPlayedGames(req, res) {
     const playedGames = await getRecentPlayedGames();
+    if (CORS_HOST) {
+        res.setHeader('Access-Control-Allow-Origin', CORS_HOST);
+        res.setHeader('Access-Control-Allow-Methods', 'GET');
+    }
     res.setHeader('Content-Type', 'application/json');
     res.setHeader('Cache-Control', 's-maxage=1');
     res.status(200).send(playedGames);
--- consts.js
+++ consts.js
@@ -1,7 +1,8 @@
-const { STEAM_API_KEY, STEAM_ID } = process.env;
+const { STEAM_API_KEY, STEAM_ID, API_CORS_HOST } = process.env;
 Object.entries({
     STEAM_API_KEY,
-    STEAM_ID
+    STEAM_ID,
+    API_CORS_HOST
 }).forEach(([key, value]) => {
     if (typeof value === "string") {
         return;
@@ -11,4 +12,5 @@ Object.entries({
 export const GET_PLAYER_SUMMARIES = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/"
 export const GET_RECENT_PLAYED_GAMES = "http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/"
 export const API_KEY = STEAM_API_KEY ?? ""
+export const CORS_HOST = API_CORS_HOST  ?? "*"
 export const ID = STEAM_ID ?? ""
